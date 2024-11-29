18c91cdddc3bcd6f822a156f1ab082f41e91443d
maria5321 <maria.silva718978@gmail.com>
2024-11-29 15:27:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que algoritmo?',
+    'algoritmos são sequências finitas de instruções'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de São Paulo?',
+    'A capital da São Paulo é São Paulo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz até mais em Inglês?',
+    'Oi em ingles é until later'
+)
